9a842314595db8e9077a2d713956f790025f6811
Subh-rgb <66378576+Subh-rgb@users.noreply.github.com>
2021-04-14 20:00:48 +0530

    Add files via upload

---
+++ AngryBirdsStage6-master/Bird.js
@@ -0,0 +1,24 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory = []
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+    if(this.body.velocity.x > 10 && this.body.position.x>200 ){
+      var position = [this.body.position.x , this.body.position.y]
+      this.trajectory.push(position) 
+    }
+
+    
+    for(var r = 0;r<this.trajectory.length;r++){
+      image(this.smokeImage,this.trajectory[r][0],this.trajectory[r][1])
+    }
+  }
+}
